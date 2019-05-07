a0157acf539895962423f72caf195c969010e292
Daniel Prince <danielahprince@live.com>
2019-05-07 23:58:48 +0200

    Implemented pencil, added transition time to paint.

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 let container:HTMLElement = document.querySelector(".sketch-container");
-let containerWidth = window.innerWidth / 1.5;
-let containerHeight = window.innerHeight / 1.5;
-container.style.width = containerWidth + "px";
-container.style.height = containerHeight + "px";
 
+// Basically fullscreens the container div
+let windowWidth = window.innerWidth;
+let windowHeight = window.innerHeight;
+container.style.width = windowWidth + "px";
+container.style.height = windowHeight + "px";
+
+// Dummy values for now.
 const gridSize = 50;
+const pencilColor = "magenta";
+
+// Calculates the size of each cell and creates css grid using repeat()
 let cellWidth = container.clientWidth / gridSize;
 let cellHeight = container.clientHeight / gridSize;
 container.style.gridTemplateColumns = `repeat(${gridSize}, ${cellWidth}px)`
@@ -14,6 +20,10 @@ container.style.gridTemplateRows = `repeat(${gridSize}, ${cellHeight}px)`
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         let gridSquare:HTMLElement = document.createElement('div')
+        gridSquare.setAttribute("class", "item")
+        gridSquare.addEventListener("mouseover", (e) => {
+            changeColorOfElement(e.target);
+        })
         let c1 = i / gridSize * 255;
         let c2 = j / gridSize * 255;
         let c3 = 255 - c1
@@ -21,3 +31,10 @@ for (let i = 0; i < gridSize; i++) {
         container.appendChild(gridSquare);
     }
 }
+
+
+
+function changeColorOfElement(div:HTMLElement) {
+    div.style.backgroundColor = pencilColor;
+}
+
